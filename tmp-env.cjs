f2f35c0ead6f6c7f30ec40d7a3d43487089f20af
aboazzam <malmohaimeed@gmail.com>
2026-09-01 05:46:01 +0300

    حذف ملفَّي عمل مؤقتين دخلا الالتزام سهوًا

--- tmp-env.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require("fs");
-for (const line of fs.readFileSync(".env.local", "utf8").split("\n")) {
-  const m = line.match(/^([A-Z0-9_]+)=(.*)$/);
-  if (m) process.env[m[1]] = m[2].trim().replace(/^"|"$/g, "");
-}
